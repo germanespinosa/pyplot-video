5b7a37d4a58bbbabb281ebe844a927f3ec3c2bab
German Espinosa <germanespinosa@gmail.com>
2022-07-17 16:28:46 -0500

    first version

---
+++ __info__.py
@@ -0,0 +1,64 @@
+def __module_version__():
+	return 0, 0, 3 
+
+
+def __module_name__():
+	return 'pyplot-video' 
+
+
+def __author__():
+	return 'German Espinosa' 
+
+
+def __author_email__():
+	return 'germanespinosa@gmail.com' 
+
+
+def __package_description__():
+	return '' 
+
+
+def __install_requires__():
+	return ['matplotlib'] 
+
+
+def __url__():
+	return '' 
+
+
+def __license__():
+	return 'MIT' 
+
+
+def __license_file__():
+	return '../LICENSE' 
+
+
+def __readme_file__():
+	return '../README.md' 
+
+
+def __package_name__():
+	return 'pyplot-video' 
+
+
+def __files__():
+	return [] 
+
+
+def __setup_py__():
+	return '../setup/setup.py' 
+
+
+def __setup_cfg__():
+	return '../setup/setup.cfg' 
+
+
+def __root_folder__():
+	return 'src' 
+
+
+def __description__():
+	return '' 
+
+
---
+++ setup/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='pyplot-video',author='German Espinosa',author_email='germanespinosa@gmail.com',packages=['pyplot-video'],install_requires=['matplotlib'],license='MIT',version='0.0.2',zip_safe=False)
